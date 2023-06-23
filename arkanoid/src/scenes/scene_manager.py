c713a44034d8b63db464adad185e1c465c4a3c98
b4kii <dami4nhalibart@gmail.com>
2023-06-23 21:43:22 +0200

    delete folder

--- arkanoid/src/scenes/scene_manager.py
+++
@@ -1,8 +0,0 @@
-class SceneManager():
-    def __init__(self, scene):
-        self.go_to(scene())
-    
-    def go_to(self, scene):
-        self.scene = scene
-        self.scene.manager = self
-    
